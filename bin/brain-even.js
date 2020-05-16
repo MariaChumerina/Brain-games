b54d8040332b068ccc34ef7b9a497e29e3006b8e
MariaChumerina <masha.chumerina@gmail.com>
2020-05-16 17:48:09 +0500

    refactor

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import { greeting } from '../src/cli.js';
-import logic from '../src/games/brain-even/logic.js';
+import play from '../src/games/brain-even/play.js';
 
 greeting();
-logic();
+play();
--- src/games/brain-even/checkAnswer.js
+++ src/games/brain-even/checkAnswer.js
@@ -3,22 +3,22 @@ import { userName } from '../../cli.js';
 
 export default function checkAnswer() {
   let number;
+
   const randomNumber = () => {
     number = Math.floor(Math.random() * 101);
     return number;
   };
 
-  function isEven() {
-    return number % 2 === 0;
-  }
+  const isEven = () => number % 2 === 0;
 
   console.log(`Question: ${randomNumber()}`);
   const answer = readlineSync.question('Your answer: ');
   const isAnswerRight = () => {
     if ((answer === 'yes' && isEven(number) === true) || (answer === 'no' && isEven(number) === false)) {
+      console.log('Correct!');
       return true;
     }
-    console.log(`${answer} is wrong answer ;(. Correct answer was "no".
+    console.log(`"${answer}" is wrong answer ;(. Correct answer was "no".
 Let's try again, ${userName()}!`);
     return false;
   };
