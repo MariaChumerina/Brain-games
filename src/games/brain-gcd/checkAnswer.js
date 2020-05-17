a1e0b409eb36f7d162177745310b3c625ee23f83
MariaChumerina <masha.chumerina@gmail.com>
2020-05-17 17:58:46 +0500

    refactor calcTrueAnswer

--- src/games/brain-gcd/checkAnswer.js
+++ src/games/brain-gcd/checkAnswer.js
@@ -1,12 +1,11 @@
 import { userName } from '../../cli.js';
 
 export default function checkAnswer(answer, condition) {
-  const numbers = condition.split(' ').sort();
+  const numbers = condition.split(' ');
   function calcTrueAnswer() {
-    const firstNumber = Number(numbers[1]);
-    const secondNumber = Number(numbers[0]);
-    if (secondNumber % firstNumber === 0) return firstNumber;
-    for (let i = firstNumber; i > 1; i -= 1) {
+    const firstNumber = Number(numbers[0]);
+    const secondNumber = Number(numbers[1]);
+    for (let i = secondNumber; i > 1; i -= 1) {
       if (secondNumber % i === 0 && firstNumber % i === 0) {
         return i;
       }
