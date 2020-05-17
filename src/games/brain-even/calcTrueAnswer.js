5b1a22e8df761f639a8631feb55a1e5082f9498b
MariaChumerina <masha.chumerina@gmail.com>
2020-05-17 23:24:54 +0500

    fix error of linter

--- src/games/brain-even/calcTrueAnswer.js
+++ src/games/brain-even/calcTrueAnswer.js
@@ -1,6 +1,6 @@
 export default function calcTrueAnswer(condition) {
-    if (condition % 2 === 0) {
-      return 'yes';
-    }
-    return 'no';
+  if (condition % 2 === 0) {
+    return 'yes';
+  }
+  return 'no';
 }
