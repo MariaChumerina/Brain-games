31bc379ec72fd225689b50a006cdedcf6f7b5244
MariaChumerina <masha.chumerina@gmail.com>
2020-05-18 15:42:19 +0500

    rename brain-prime.js to brainPrime.js

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -2,7 +2,7 @@
 import index from '../src/index.js';
 import { getRandomNumber } from '../src/questions.js';
 import { rules } from '../src/cli.js';
-import isPrime from '../src/games/brain-prime/isPrime.js';
+import isPrime from '../src/games/brainPrime/isPrime.js';
 
 
 index(rules.brainPrime, getRandomNumber, isPrime);
